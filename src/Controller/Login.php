24ce4865d9b8b711e13f2d25f7ff8a7856fff11f
BORSCHELLO <aleksandrnzga@gmail.com>
2020-08-06 07:59:05 +0000

    session and logout

--- src/Controller/Login.php
+++ src/Controller/Login.php
@@ -15,34 +15,33 @@ class Login extends AbstractController
      *@Route("/", name="login")
      */
     public function login(Request $request)
-
     {
+        $session = new Session();
         $user = new User();
         $user->setName('');
-        $form = $this->createFormBuilder($user)
+                $form = $this->createFormBuilder($user)
             ->add('name', TextType::class,
                 array('attr' => array('class' => "form-control")))
             ->add('save', SubmitType::class,
                 array('label' => 'Отправить', 'attr' => array('class' => "btn btn-success mt-2")))
             ->getForm();
         $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid() ) {
-            $requestUser = $form->getData();
-            $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['name' => $requestUser-> getName ()]);
-
-            $_SESSION["user"]=$requestUser-> getName ();
-
-            if($user){
-                $this->addFlash('success','С возвращением'.' '.$requestUser->getName());
-                return $this->redirectToRoute('main');
-            }else{
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($requestUser);
-                $this->addFlash('success','Вы удачно зарегистрированы'.' '.$requestUser->getName());
-                $em->flush();
-                return $this->redirectToRoute('main');
-            }
+            if ($form->isSubmitted() && $form->isValid() ) {
+                $requestUser = $form->getData();
+                $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['name' => $requestUser-> getName ()]);
+                if($user){
+                    $this->addFlash('success','С возвращением'.' '.$requestUser->getName());
+                    $session->set('user', $user->getId());
+                    return $this->redirectToRoute('main');
+                }else{
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($requestUser);
+                    $this->addFlash('success','Вы удачно зарегистрированы'.' '.$requestUser->getName());
+                    $em->flush();
+                    $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['name' => $requestUser-> getName ()]);
+                    $session->set('user', $user->getId());
+                    return $this->redirectToRoute('main');
+                }
         }
             return $this->render('login.html.twig', array(
             'form' => $form->createView()
@@ -54,9 +53,16 @@ class Login extends AbstractController
      */
     public function main()
     {
-
         return $this->render('main.html.twig');
     }
-
+    /**
+     *@Route("#", name="logout")
+     */
+    public function logout()
+    {
+        $session=new Session();
+        $session->remove('user');
+        return $this->redirectToRoute('login');
+    }
 }
 
